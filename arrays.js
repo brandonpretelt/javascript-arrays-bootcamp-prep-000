444f08f8034fc78f941c814f020dd37a9f285061
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 09:12:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]; 
 }
+
+function destuctivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
