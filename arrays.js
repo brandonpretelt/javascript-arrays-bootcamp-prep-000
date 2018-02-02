d1a69a9197408f4b38959db86b0f2628de2b7ef8
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 08:13:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+function aAddElementToBeginningOfArray(arr, el) {
+  var newArr = arr; 
+  newArr.unshift(el);
+  return newArr;
+}
