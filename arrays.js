a9d526cc9bb84ddb2f6cfc29c441d2b44efe99be
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-02 09:29:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,9 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
- /* var arr = array;
+ var arr = array;
   arr.unshift(element);
-  return arr; */
+  return arr; 
 }
 
 function addElementToEndOfArray(array, element) {
